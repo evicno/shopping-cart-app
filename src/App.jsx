6715545b09f001b8335b6aa67a3d449d62e0031f
evicno <229501549+evicno@users.noreply.github.com>
2026-06-18 20:03:29 +0200

    Add Shop button to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useNavigate } from 'react-router';
 import './App.css';
 import Nav from './components/Nav';
 
 function App() {
+  let navigate = useNavigate();
   return (
     <>
       <Nav />
       <h1>Shopping cart</h1>
+      <button onClick={() => navigate('/shop')}>Shop</button>
     </>
   );
 }
